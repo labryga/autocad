d9e76b08b46c8e9afdb7e1afe9d96f4b7576044b
Marcin Labryga <martin@labryga.ch>
2019-10-02 13:58:30 +0200

    level file creation updated

--- scr/create_level_scr.py
+++ scr/create_level_scr.py
@@ -6,7 +6,7 @@ with open("create_dwg_levels.scr", "w") as script_file:
     for i in range(1, 15):
         geschossnumer = "0" + str(i) if len(str(i)) < 2 else str(i)
         script_file.write("save " + \
-                          "\"" + \
+                          "\"C:\\" + \
                           geschossnumer + \
                           "_og.dwg\"\n")
 
