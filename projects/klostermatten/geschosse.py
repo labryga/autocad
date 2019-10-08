48dda0aa4487e19ac8012fc9e3ae462e2a626d7f
labryga <marcin@labryga.ch>
2019-10-08 11:09:21 +0200

    raumnamen und raumnummer begefügt; struktur angepasst

--- projects/klostermatten/geschosse.py
+++ projects/klostermatten/geschosse.py
@@ -8,11 +8,15 @@ raume_normgeschoss = {1: "Korridor",
 with open("raumliste.csv", "w") as raumliste:
 
     for geschoss in range(15):
+        raumliste.write("OG {:02d}\n\n".format(geschoss))
         for wohnung in range(1, 5):
+            raumliste.write("Wohnung {:02d}{}\n\n".format(geschoss, wohnung))
             for raumnummer, raumname in raume_normgeschoss.items():
                 if wohnung in [3,4] and raumnummer == 6: continue
-                raumliste.write("{:02d}{}.{:02d}\n".format(geschoss,
+                raumliste.write("{:02d}{}.{:02d}\t{}\n".format(geschoss,
                                                            wohnung,
-                                                           raumnummer))
+                                                           raumnummer,
+                                                           raumname))
+            raumliste.write("\n")
 
 
