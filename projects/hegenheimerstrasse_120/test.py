3e084f65709ae8404e8638c634dc5a52a0970988
Marcin Labryga <martin@labryga.ch>
2019-12-17 10:27:53 +0100

    wand_element variable added

--- projects/hegenheimerstrasse_120/test.py
+++ projects/hegenheimerstrasse_120/test.py
@@ -8,9 +8,9 @@ with open(dateipfad) as json_file:
 entries_list = []
 
 for eintrag in json_data["waende"]:
-  for element in eintrag.keys():
-    if element not in entries_list:
-        entries_list.append(element)
+  for wand_element in eintrag.keys():
+    if wand_element not in entries_list:
+        entries_list.append(wand_element)
 
 excel_file = openpyxl.Workbook()
 excel_sheet = excel_file.active
@@ -24,32 +24,31 @@ for alphabeticals in column_index:
 
 zeilen_nummer = 3
 
-
 for eintrag in json_data["waende"]:
 
-    for element in entries_list[:6]:
-        excel_sheet[column_index[entries_list.index(element)] + \
-            str(zeilen_nummer)] = eintrag[element]
+    for wand_element in entries_list[:6]:
+        excel_sheet[column_index[entries_list.index(wand_element)] + \
+            str(zeilen_nummer)] = eintrag[wand_element]
 
     zeilen_nummer += 1
 
     for exemplar in eintrag["exemplar_nummern"]:
-        for element in entries_list:
+        for wand_element in entries_list:
 
-            if not element == "bkp-nr":
+            if not wand_element == "bkp-nr":
                 try:
-                    cell_value = float(eintrag[element])
+                    cell_value = float(eintrag[wand_element])
                 except:
-                    cell_value = eintrag[element]
+                    cell_value = eintrag[wand_element]
             else:
-                cell_value = eintrag[element]
+                cell_value = eintrag[wand_element]
 
-            excel_sheet[column_index[entries_list.index(element)] + \
+            excel_sheet[column_index[entries_list.index(wand_element)] + \
                 str(zeilen_nummer)] = (cell_value,
                                          eintrag["wandtyp"] + "-" + exemplar) \
-                                        [element == "exemplar_nummern"]
+                                        [wand_element == "exemplar_nummern"]
 
-            _cell_value = excel_sheet[column_index[entries_list.index(element)] + str(zeilen_nummer)]
+            _cell_value = excel_sheet[column_index[entries_list.index(wand_element)] + str(zeilen_nummer)]
             try:
                 _cell_value.number_format = "0.00 \"m²\""
             except:
